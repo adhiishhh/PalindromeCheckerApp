6f599210b3c93d780a8af6491befddae2b204279
Adhish Kumar M T <adhishkumar488@gmail.com>
2026-03-04 22:02:20 +0530

    UC1: Application Entry and Welcome Message

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,2 +1,10 @@
 public class PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        // UC1 - Application Entry & Welcome
+        System.out.println("Welcome to the Palindrome Checker App!");
+        System.out.println("This application will help you check whether a word is a palindrome.");
+
+    }
 }
