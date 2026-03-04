837475b6058c6c8e52e2b7e514c1e9430c6527d2
Adhish Kumar M T <adhishkumar488@gmail.com>
2026-03-04 22:28:29 +0530

    UC2: Hardcoded palindrome check

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,2 +1,20 @@
 public class PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        // UC2 - Hardcoded Palindrome Check
+
+        String word = "radar";
+        String reversed = "";
+
+        for (int i = word.length() - 1; i >= 0; i--) {
+            reversed = reversed + word.charAt(i);
+        }
+
+        if (word.equals(reversed)) {
+            System.out.println(word + " is a palindrome.");
+        } else {
+            System.out.println(word + " is not a palindrome.");
+        }
+    }
 }
